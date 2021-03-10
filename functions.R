d67c3de73e9b851eadec79a7f928bc479bbb989d
Yohan Yee <yohan.yee@gmail.com>
2021-03-10 16:41:00 -0500

    All major functions for plotting registration-related figures added

--- functions.R
+++ functions.R
@@ -448,9 +448,11 @@ prepare_anatomy <- function(file, slice_axis, slice_axis_coordinates) {
                     mutate(x=w,
                            y=as.numeric(y), 
                            z=as.numeric(z),
+                           slice_axis=slice_axis,
                            slice_voxel=v,
                            slice_world=w,
-                           slice_index=index)
+                           slice_index=index) %>%
+                    select(x, y, z, intensity, slice_axis, slice_voxel, slice_world, slice_index)
                 },
                 y={
                   
@@ -465,9 +467,11 @@ prepare_anatomy <- function(file, slice_axis, slice_axis_coordinates) {
                     mutate(x=as.numeric(x), 
                            y=w,
                            z=as.numeric(z),
+                           slice_axis=slice_axis,
                            slice_voxel=v,
                            slice_world=w,
-                           slice_index=index)
+                           slice_index=index) %>%
+                    select(x, y, z, intensity, slice_axis, slice_voxel, slice_world, slice_index)
                 },
                 z={
                   
@@ -482,9 +486,11 @@ prepare_anatomy <- function(file, slice_axis, slice_axis_coordinates) {
                     mutate(x=as.numeric(x), 
                            y=as.numeric(y),
                            z=w,
+                           slice_axis=slice_axis,
                            slice_voxel=v,
                            slice_world=w,
-                           slice_index=index)
+                           slice_index=index) %>%
+                    select(x, y, z, intensity, slice_axis, slice_voxel, slice_world, slice_index)
                 }
         )
         
@@ -500,6 +506,224 @@ prepare_anatomy <- function(file, slice_axis, slice_axis_coordinates) {
               anatomy_df=anatomy_df,
               slice_axis=slice_axis,
               slice_axis_coordinates=slice_axis_coordinates)
+}
+
+prepare_masked_anatomy <- function(file, mask_file, slice_axis, slice_axis_coordinates) {
+  
+  # Read data
+  anatomy_data <- prepare_anatomy(file, slice_axis = slice_axis, slice_axis_coordinates = slice_axis_coordinates)
+  mask_data <- prepare_anatomy(mask_file, slice_axis = slice_axis, slice_axis_coordinates = slice_axis_coordinates)
+  
+  # Merge mask data
+  anatomy_and_mask_df <- anatomy_data$anatomy_df %>%
+    left_join(mask_data$anatomy_df %>%
+                select(x, y, z, mask_value=intensity), 
+              by=c("x", "y", "z")) %>%
+    select(x, y, z, intensity, mask_value, everything())
+  
+  # Output
+  out <- list(file=file,
+              anatomy_df=anatomy_and_mask_df,
+              slice_axis=slice_axis,
+              slice_axis_coordinates=slice_axis_coordinates)
+  
+  return(out)
+
+}
+
+#############################
+
+prepare_contours <- function(file, slice_axis, slice_axis_coordinates, contour_levels, mask_file=NULL, filter_size=NULL) {
+  
+  # Get file shape
+  file_shape <- get_file_shape(file)
+  
+  # Get voxel indices
+  slices_df <- get_slice_mappings(file = file, slice_axis = slice_axis, slice_axis_coordinates = slice_axis_coordinates)
+  
+  # Read in 3D file
+  arr <- mincArray(mincGetVolume(file))
+  if (!is.null(mask_file)) {
+    maskarr <- mincArray(mincGetVolume(mask_file))
+  }
+  
+  # Get array squence based on step direction
+  # This is so that we can flip array based on direction 
+  # (a workaround since contourLines requires x, y array coordinates be provided in ascending order, but step direction may be negative)
+  axis_sequence <- 1:3 %>%
+    map(function(ax) {
+      ax_dir <- file_shape$direction[[ax]]
+      if (ax_dir == -1) {
+        ax_seq <- rev(1:dim(arr)[[ax]])
+      } else {
+        ax_seq <- 1:dim(arr)[[ax]]
+      }
+    })
+  
+  # Get slices
+  contours_df <- seq_along(slices_df$slice_index) %>%
+    map_dfr(
+      function(i) {
+        
+        # Get world and voxel coordinates corresponding to slice
+        w <- slices_df$world[i]
+        v <- slices_df$voxel[i]
+        index <- slices_df$slice_index[i]
+        
+        # Get 2D array corresponding to background intensities
+        switch (slice_axis,
+                x={
+                  
+                  # Get slice array
+                  slice_array <- arr[(v + 1),,]
+                  if (!is.null(mask_file)) {
+                    mask_array <- maskarr[(v + 1),,]
+                    slice_array[mask_array < 0.5] <- 0
+                  }
+                  
+                  # Flip array according to direction
+                  slice_array <- slice_array[axis_sequence[[2]], axis_sequence[[3]]]
+                  if (!is.null(mask_file)) {
+                    mask_array <- mask_array[axis_sequence[[2]], axis_sequence[[3]]]
+                  }
+                  
+                  # Use grDevices::contourLines() to get the contours
+                  # Returns a list of contour paths
+                  slice_contours <- contourLines(x = seq(from=file_shape$starts["y"], 
+                                                         to=file_shape$ends["y"], 
+                                                         length.out = dim(arr)[[2]]), 
+                                                 y = seq(from=file_shape$starts["z"], 
+                                                         to=file_shape$ends["z"], 
+                                                         length.out = dim(arr)[[3]]), 
+                                                 z = slice_array, 
+                                                 levels = contour_levels)
+                  
+                  # Wrangle data to long form
+                  out <- 1:length(slice_contours) %>%
+                    map(function(i) {
+                      as_tibble(slice_contours[[i]]) %>%
+                        mutate(obj=i)
+                    }
+                    ) %>%
+                    bind_rows() %>%
+                    rename(y=x, z=y) %>%
+                    mutate(x=w, 
+                           slice_axis=slice_axis,
+                           slice_voxel=v,
+                           slice_world=w,
+                           slice_index=index) %>%
+                    select(level, obj, x, y, z, slice_axis, slice_voxel, slice_world, slice_index)
+                },
+                y={
+                  
+                  # Get slice array
+                  slice_array <- arr[,(v + 1),]
+                  if (!is.null(mask_file)) {
+                    mask_array <- maskarr[,(v + 1),]
+                    slice_array[mask_array < 0.5] <- 0
+                  }
+                  
+                  # Flip array according to direction
+                  slice_array <- slice_array[axis_sequence[[1]], axis_sequence[[3]]]
+                  if (!is.null(mask_file)) {
+                    mask_array <- mask_array[axis_sequence[[1]], axis_sequence[[3]]]
+                  }
+                  
+                  # Use grDevices::contourLines() to get the contours
+                  # Returns a list of contour paths
+                  slice_contours <- contourLines(x = seq(from=file_shape$starts["x"], 
+                                                         to=file_shape$ends["x"], 
+                                                         length.out = dim(arr)[[1]]), 
+                                                 y = seq(from=file_shape$starts["z"], 
+                                                         to=file_shape$ends["z"], 
+                                                         length.out = dim(arr)[[3]]), 
+                                                 z = slice_array, 
+                                                 levels = contour_levels)
+                  
+                  # Wrangle data to long form
+                  out <- 1:length(slice_contours) %>%
+                    map(function(i) {
+                      as_tibble(slice_contours[[i]]) %>%
+                        mutate(obj=i)
+                    }
+                    ) %>%
+                    bind_rows() %>%
+                    rename(x=x, z=y) %>%
+                    mutate(y=w, 
+                           slice_axis=slice_axis,
+                           slice_voxel=v,
+                           slice_world=w,
+                           slice_index=index) %>%
+                    select(level, obj, x, y, z, slice_axis, slice_voxel, slice_world, slice_index)
+                },
+                z={
+                  
+                  # Get slice array
+                  slice_array <- arr[,,(v + 1)]
+                  if (!is.null(mask_file)) {
+                    mask_array <- maskarr[,,(v + 1)]
+                    slice_array[mask_array < 0.5] <- 0
+                  }
+                  
+                  # Flip array according to direction
+                  slice_array <- slice_array[axis_sequence[[1]], axis_sequence[[2]]]
+                  if (!is.null(mask_file)) {
+                    mask_array <- mask_array[axis_sequence[[1]], axis_sequence[[2]]]
+                  }
+                  
+                  # Use grDevices::contourLines() to get the contours
+                  # Returns a list of contour paths
+                  slice_contours <- contourLines(x = seq(from=file_shape$starts["x"], 
+                                                         to=file_shape$ends["x"], 
+                                                         length.out = dim(arr)[[1]]), 
+                                                 y = seq(from=file_shape$starts["y"], 
+                                                         to=file_shape$ends["y"], 
+                                                         length.out = dim(arr)[[2]]), 
+                                                 z = slice_array, 
+                                                 levels = contour_levels)
+                  
+                  # Wrangle data to long form
+                  out <- 1:length(slice_contours) %>%
+                    map(function(i) {
+                      as_tibble(slice_contours[[i]]) %>%
+                        mutate(obj=i)
+                    }
+                    ) %>%
+                    bind_rows() %>%
+                    rename(x=x, y=y) %>%
+                    mutate(z=w, 
+                           slice_axis=slice_axis,
+                           slice_voxel=v,
+                           slice_world=w,
+                           slice_index=index) %>%
+                    select(level, obj, x, y, z, slice_axis, slice_voxel, slice_world, slice_index)
+                }
+        )
+        
+        
+        
+        # Return 
+        return(out)
+      }
+    )
+  
+  # Filter by size if required
+  # Many contour paths exist. Remove the small ones by filtering out paths with few points
+  if (!is.null(filter_size)) {
+    contours_df <- contours_df %>%
+      group_by(obj, slice_index) %>%
+      filter(length(level) >= filter_size) %>%
+      ungroup()
+  }
+  
+  # Return
+  out <- list(file=file,
+              contours_df=contours_df,
+              slice_axis=slice_axis,
+              slice_axis_coordinates=slice_axis_coordinates,
+              contour_levels=contour_levels,
+              mask_file=mask_file,
+              filter_size=filter_size)
 }
 
 #############################
@@ -1060,6 +1284,42 @@ study_mask_file = "resources/HLHS/study_mask.mnc"
 native_to_lsq6_xfm = "resources/HLHS/subject1_lsq6.xfm"
 lsq6_to_nlin_xfm = "resources/HLHS/subject1_N_I_lsq6_lsq12_and_nlin.xfm"
 nlin_abs_jd_file = "resources/HLHS/subject1_absjd.mnc"
+grid_padding = 0.5
+line_points = 200
+grid_spacing = 0.2
+highres_grid_lines = 12
+tmpdir = "/tmp"
+plot_progress=T
+
+subject <- "subject10"
+slice_axis = "y"
+nlin_slice_axis_coordinate = -0.288
+native_file = glue("resources/HLHS/{subject}_native.mnc")
+lsq6_file = glue("resources/HLHS/{subject}_lsq6.mnc")
+nlin_file = glue("resources/HLHS/{subject}_nlin.mnc")
+study_template_file = "resources/HLHS/study_template.mnc"
+study_mask_file = "resources/HLHS/study_mask.mnc"
+native_to_lsq6_xfm = glue("resources/HLHS/{subject}_lsq6.xfm")
+lsq6_to_nlin_xfm = glue("resources/HLHS/{subject}_N_I_lsq6_lsq12_and_nlin.xfm")
+nlin_abs_jd_file = glue("resources/HLHS/{subject}_absjd.mnc")
+grid_padding = 0.5
+line_points = 200
+grid_spacing = 0.2
+highres_grid_lines = 12
+tmpdir = "/tmp"
+plot_progress=T
+
+subject <- "subject10"
+slice_axis = "y"
+nlin_slice_axis_coordinate = -0.288
+native_file = ""
+lsq6_file = glue("/hpf/largeprojects/MICe/arahman/PhD/CHD_mutants/HLHS_surgical/papertwo_experiments/MRI/MRI_brains/registration_jan132021/hlhs_jan132021_processed/{subject}/resampled/{subject}_N_I_lsq6.mnc")
+nlin_file = glue("/hpf/largeprojects/MICe/arahman/PhD/CHD_mutants/HLHS_surgical/papertwo_experiments/MRI/MRI_brains/registration_jan132021/hlhs_jan132021_processed/{subject}/resampled/{subject}_N_I_lsq6_lsq12_and_nlin-resampled.mnc")
+study_template_file = "resources/HLHS/study_template.mnc"
+study_mask_file = "resources/HLHS/study_mask.mnc"
+native_to_lsq6_xfm = glue("/hpf/largeprojects/MICe/arahman/PhD/CHD_mutants/HLHS_surgical/papertwo_experiments/MRI/MRI_brains/registration_jan132021/hlhs_jan132021_processed/{subject}/transforms/{subject}_lsq6.xfm")
+lsq6_to_nlin_xfm = glue("/hpf/largeprojects/MICe/arahman/PhD/CHD_mutants/HLHS_surgical/papertwo_experiments/MRI/MRI_brains/registration_jan132021/hlhs_jan132021_processed/{subject}/transforms/{subject}_N_I_lsq6_lsq12_and_nlin.xfm")
+nlin_abs_jd_file = glue("/hpf/largeprojects/MICe/arahman/PhD/CHD_mutants/HLHS_surgical/papertwo_experiments/MRI/MRI_brains/registration_jan132021/hlhs_jan132021_processed/{subject}/stats-volumes/{subject}_N_I_lsq6_lsq12_and_nlin_inverted_displ_log_det_abs.mnc")
 grid_padding = 0.5
 line_points = 200
 grid_spacing = 0.2
